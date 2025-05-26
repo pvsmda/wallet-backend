965cf54e4e4b78fdc61169711f6a3dce71e78716
pvsmda <spaulovictor813@gmail.com>
2025-05-26 12:56:53 -0300

    third commit

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRouter from "./routes/transactions.route.js";
+import job from "./cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env === "production") job.start();
 
 app.use(cors());
 
@@ -23,10 +26,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
+app.length("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use("/api/transactions", transactionsRouter);
 
 initDB().then(() => {
-  app.listen(PORT, '0.0.0.0',() => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log("Server is running on port 5001");
   });
 });
